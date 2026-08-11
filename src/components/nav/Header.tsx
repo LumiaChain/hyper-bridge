9e432977bc0c00a3140e4b0e7387738f69fbf5ce
Nikita Khokhlov <kuscoder@yandex.ru>
2026-08-11 13:04:53 +0400

    Fix mobile header spacing

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -5,7 +5,7 @@ import Name from '../../images/logos/app-name.svg';
 
 export function Header() {
   return (
-    <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
+    <header className="w-full px-2 pb-2 pt-12 sm:px-6 sm:pt-3 lg:px-12">
       <div className="flex items-start justify-between">
         <Link href="/" className="flex items-center py-2">
           <Image src={Name} width={130} alt="" className="ml-2 mt-0.5 hidden sm:block" />
